e1ed6ebf28e19220de3719c77df46a54879c9d7c
Thales Salazar <thalessalazar.12@gmail.com>
2021-06-15 14:59:52 -0300

    Finalizado controlador stepper

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,8 @@
   var inputPasswordForm = document.getElementById('inputPasswordForm');
   var form = stepperFormEl.querySelector('.bs-stepper-content form');
 
+  var progressBar = document.getElementById("progressbar");
+
   btnNextList.forEach(function (btn) {
     btn.addEventListener('click', function () {
       window.stepperForm.next();
@@ -30,13 +32,19 @@
   });
 
   stepperFormEl.addEventListener('show.bs-stepper', function (event) {
+    console.log(event);
+
     form.classList.remove('was-validated');
     var nextStep = event.detail.indexStep;
     var currentStep = nextStep;
 
+    console.log(currentStep);
+
     if (currentStep > 0) {
       currentStep--;
     }
+
+    localStorage.setItem("currentStep", currentStep);
 
     var stepperPan = stepperPanList[currentStep];
 
@@ -54,28 +62,62 @@
       }
     }
 
-    
-
     if (error.length > 0) {
       event.preventDefault();
       updateErrorMsg(error);
       form.classList.add('was-validated');
+    } else {
+      if (nextStep < event.detail.from) {
+        rollbackCompleteStep(nextStep)
+        rollbackCompleteStep(currentStep)
+      } else {
+        updateCompleteStep();
+      }
+      updateProgressBar(currentStep);
     }
   });
-
 
   const updateErrorMsg = (error) => {
     console.log(error)
     error.forEach(err => {
       let valid = document.getElementById(err.id).nextSibling.nextSibling;
       let errorExisting = ""
-      
-      if(valid.innerHTML != "") {
+
+      if (valid.innerHTML != "") {
         errorExisting = valid.innerHTML;
       }
 
       valid.innerHTML = errorExisting + " " + err.err
     });
+  }
+
+  const updateProgressBar = (step) => {
+    if (step == 0) {
+      progressBar.style.width = "0%";
+    }
+
+    let [percent,] = progressBar.style.width.split("%");
+    percent = parseInt(percent);
+    percent += 33;
+
+    progressBar.style.width = `${percent}%`
+  }
+
+  const updateCompleteStep = () => {
+    const stepper = document.querySelector(".step.active")
+    stepper.classList.add("complete");
+
+    const circle = document.querySelector(".step.active .bs-stepper-circle");
+    circle.innerHTML = `<i class="fa fa-check"></i>`
+  }
+
+  const rollbackCompleteStep = (step) => {
+    const stepper = document.getElementById(`step-${step + 1}`);
+    stepper.classList.remove("complete");
+
+    const circle = document.querySelector(`#step-${step + 1} .bs-stepper-circle`);
+    console.log(circle);
+    circle.innerHTML = step + 1;
   }
 
 })();
